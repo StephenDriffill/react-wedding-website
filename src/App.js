06d5e699f9d98f361ff7f3e61c6591ec58a0709f
Stephen Driffill <sdriffill@hotmail.co.uk>
2023-01-08 21:35:47 +0000

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
+import React from "react"
 import "./App.css"
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+function App() {
 
-  handleClick = api => e => {
-    e.preventDefault()
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+  return (
+    <div className="App">
 
-  render() {
-    const { loading, msg } = this.state
+      {matches ? (<div className="smooth">
+        <div className="header">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#itinerary">On the day</a></li>
+            <li><a href="#accomodation">Accomodation</a></li> 
+            <li><a href="#taxi">Taxis</a></li>
+            <li><a href="#rsvp">RSVP</a></li>
+          </ul>
+        </div>
+      </div>) : null}
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+      <section id="home">
+        <div className="section-content App__headline">
+          <span className="App__headlineName">Amelia & Steve</span>
+          <span className="App__headlineDate">{`24${'\u00B7'}06${'\u00B7'}23`}</span>
+          <span className="App__headlineLocation">Frome, Somerset</span>
+        </div>
+      </section>
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
+      <section id="itinerary">
+        <div className="section-content App__itinerary">
+          <span>On the day...</span>
+          <div className="App__itineraryContent">
+            <span>14:15</span><span>Arrive at town hall</span>
+            <span>14:30</span><span>Service</span>
+            <span>15:00</span><span>Tea and cake</span>
+            <span>19:00</span><span>Dinner</span>
+          </div>
+        </div>
+      </section>
+
+      <section id="rsvp">
+        <div className="section-content App__rsvp">
+          <span>Please RSVP to</span>
+          <span className="App__rsvpEmail"><a href="mailto:ameliaandstevegettingmarried@gmail.com">ameliaandstevegettingmarried@gmail.com</a></span>
+          <span>by 31st March.</span>
+        </div>
+      </section>
+
+    </div>
+  )
 }
 
 export default App
