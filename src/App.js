1dccade20b20ebd9d0bc9227d345771a36fde898
Stephen Driffill <sdriffill@hotmail.co.uk>
2023-01-09 23:43:12 +0000

    Fix build error.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
           <span>On the day...</span>
 
           <span className="App__itineraryLocation">
-            <a href="https://goo.gl/maps/VjexHNM4AVH4DFN59" target="_blank">Frome Town Hall</a>
+            <a href="https://goo.gl/maps/VjexHNM4AVH4DFN59" target="_blank" rel="noopener noreferrer">Frome Town Hall</a>
           </span>
           <div className="App__itineraryContent">
             <span>14:15</span><span>Guests arrive</span>
@@ -55,14 +55,14 @@ function App() {
           </div>
 
           <span className="App__itineraryLocation">
-            <a href="https://goo.gl/maps/tRoZHUijFRv9bbd88" target="_blank">4 Weston Walk</a>
+            <a href="https://goo.gl/maps/tRoZHUijFRv9bbd88" target="_blank" rel="noopener noreferrer">4 Weston Walk</a>
           </span>
           <div className="App__itineraryContent">
             <span>15:30</span><span>Tea and cake</span>
           </div>
             
           <span className="App__itineraryLocation">
-            <a href="https://goo.gl/maps/QyfShK7KULminBn87" target="_blank">Eight Stony Street</a>
+            <a href="https://goo.gl/maps/QyfShK7KULminBn87" target="_blank" rel="noopener noreferrer">Eight Stony Street</a>
           </span>
           <div className="App__itineraryContent">
             <span>19:00</span><span>Dinner</span>
@@ -76,37 +76,37 @@ function App() {
             <ul>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/4qZzqYgQm8prGkXN7" target="_blank">The Cornerhouse</a>
+                  <a href="https://goo.gl/maps/4qZzqYgQm8prGkXN7" target="_blank" rel="noopener noreferrer">The Cornerhouse</a>
                   <a href="tel:+441373472042">01373 472042</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/tf8aXTMKmuBX8DMT6" target="_blank">Bistro Lotte</a>
+                  <a href="https://goo.gl/maps/tf8aXTMKmuBX8DMT6" target="_blank" rel="noopener noreferrer">Bistro Lotte</a>
                   <a href="tel:+441373300646">01373 300646</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/VmYJyfcV7V9MS16i9" target="_blank">The George Hotel & Granary</a>
+                  <a href="https://goo.gl/maps/VmYJyfcV7V9MS16i9" target="_blank" rel="noopener noreferrer">The George Hotel & Granary</a>
                   <a href="tel:+441373462584">01373 462584</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/xw81VNDz4LdK8i4t9" target="_blank">Premier Inn Frome</a>
+                  <a href="https://goo.gl/maps/xw81VNDz4LdK8i4t9" target="_blank" rel="noopener noreferrer">Premier Inn Frome</a>
                   <a href="tel:+443337777269">0333 777 7269</a>
                 </div>
               </li>
               
               <li>
                 <div>
-                  <a href="https://www.airbnb.co.uk/s/Frome--England--United-Kingdom/homes?adults=2&checkin=2023-06-24&checkout=2023-06-25" target="_blank">Airbnb</a>
+                  <a href="https://www.airbnb.co.uk/s/Frome--England--United-Kingdom/homes?adults=2&checkin=2023-06-24&checkout=2023-06-25" target="_blank" rel="noopener noreferrer">Airbnb</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/hDgUqTwqewbc3WYQ9" target="_blank">Other</a>
+                  <a href="https://goo.gl/maps/hDgUqTwqewbc3WYQ9" target="_blank" rel="noopener noreferrer">Other</a>
                 </div>
               </li>
             </ul>
@@ -119,37 +119,37 @@ function App() {
             <ul>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/WuyPoKYiQKodFsxD9" target="_blank">Mike Taxis</a>
+                  <a href="https://goo.gl/maps/WuyPoKYiQKodFsxD9" target="_blank" rel="noopener noreferrer">Mike Taxis</a>
                   <a href="tel:+447773900274">07773 900274</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/LcdafUUdQ5tVqWWt8" target="_blank">Tony’s Cabs Ltd</a>
+                  <a href="https://goo.gl/maps/LcdafUUdQ5tVqWWt8" target="_blank" rel="noopener noreferrer">Tony’s Cabs Ltd</a>
                   <a href="tel:+441373469898">01373 469898</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/6VRWp5mLxqXUHrUs5" target="_blank">M and H Taxis</a>
+                  <a href="https://goo.gl/maps/6VRWp5mLxqXUHrUs5" target="_blank" rel="noopener noreferrer">M and H Taxis</a>
                   <a href="tel:+447768856327">07768 856327</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/Yj4MFjb5DnxYkJLT7" target="_blank">Smile Taxis</a>
+                  <a href="https://goo.gl/maps/Yj4MFjb5DnxYkJLT7" target="_blank" rel="noopener noreferrer">Smile Taxis</a>
                   <a href="tel:+447931893636">07931 893636</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/Yj4MFjb5DnxYkJLT7" target="_blank">Crazy Cabs Taxi</a>
+                  <a href="https://goo.gl/maps/Yj4MFjb5DnxYkJLT7" target="_blank" rel="noopener noreferrer">Crazy Cabs Taxi</a>
                   <a href="tel:+441373466000">01373 466000</a>
                 </div>
               </li>
               <li>
                 <div>
-                  <a href="https://goo.gl/maps/DLh3HtyBSfuTqaJa9" target="_blank">Other</a>
+                  <a href="https://goo.gl/maps/DLh3HtyBSfuTqaJa9" target="_blank" rel="noopener noreferrer">Other</a>
                 </div>
               </li>
             </ul>
