1019f9e9c396c1bdd62aa061c54f85af52b93d80
Stephen Driffill <sdriffill@hotmail.co.uk>
2023-01-09 23:26:34 +0000

    Various improvements.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import React from "react"
 import "./App.css"
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import HideAppBar from "./HideAppBar";
 
 function App() {
 
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+  const smMatches = useMediaQuery(theme.breakpoints.up('sm'));
+  const mdMatches = useMediaQuery(theme.breakpoints.up('md'));
+
+  const sizeClassName = mdMatches ? 'md' : smMatches ? 'sm' : 'xs';
 
   return (
     <div className="App">
+      {
+        mdMatches 
+          ? (
+            <div className="smooth">
+              <div className="header">
+                <ul>
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#itinerary">On the day</a></li>
+                  <li><a href="#accomodation">Accomodation</a></li> 
+                  <li><a href="#taxis">Taxis</a></li>
+                  <li><a href="#rsvp">RSVP</a></li>
+                </ul>
+              </div>
+            </div>
+          ) 
+          : (
+            <HideAppBar/>
+          )
+        }
 
-      {matches ? (<div className="smooth">
-        <div className="header">
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#itinerary">On the day</a></li>
-            <li><a href="#accomodation">Accomodation</a></li> 
-            <li><a href="#taxis">Taxis</a></li>
-            <li><a href="#rsvp">RSVP</a></li>
-          </ul>
-        </div>
-      </div>) : null}
-
-      <section id="home">
+      <section id="home" className={sizeClassName}>
         <div className="section-content App__headline">
           <span className="App__headlineName">Amelia & Steve</span>
           <span className="App__headlineDate">{`24${'\u00B7'}06${'\u00B7'}23`}</span>
@@ -35,41 +46,111 @@ function App() {
         <div className="section-content App__itinerary">
           <span>On the day...</span>
 
-          <span className="App__itineraryLocation">Frome Town Hall</span>
+          <span className="App__itineraryLocation">
+            <a href="https://goo.gl/maps/VjexHNM4AVH4DFN59" target="_blank">Frome Town Hall</a>
+          </span>
           <div className="App__itineraryContent">
             <span>14:15</span><span>Guests arrive</span>
             <span>14:30</span><span>Ceremony</span>
           </div>
 
-          <span className="App__itineraryLocation">4 Weston Walk</span>
+          <span className="App__itineraryLocation">
+            <a href="https://goo.gl/maps/tRoZHUijFRv9bbd88" target="_blank">4 Weston Walk</a>
+          </span>
           <div className="App__itineraryContent">
-            <span>15:00</span><span>Tea and cake</span>
+            <span>15:30</span><span>Tea and cake</span>
           </div>
             
-          <span className="App__itineraryLocation">Eight Stony Street</span>
+          <span className="App__itineraryLocation">
+            <a href="https://goo.gl/maps/QyfShK7KULminBn87" target="_blank">Eight Stony Street</a>
+          </span>
           <div className="App__itineraryContent">
             <span>19:00</span><span>Dinner</span>
           </div>
         </div>
       </section>
 
-      <section id="accomodation">
+      <section id="accomodation" className={sizeClassName}>
         <div className="section-content App__accomodation">
           <span>Accomodation</span>
             <ul>
               <li>
-                <a href="https://mikestaxifrome.com/">The Cornerhouse</a> - <a href="tel:+441373472042">01373 472042</a>
+                <div>
+                  <a href="https://goo.gl/maps/4qZzqYgQm8prGkXN7" target="_blank">The Cornerhouse</a>
+                  <a href="tel:+441373472042">01373 472042</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/tf8aXTMKmuBX8DMT6" target="_blank">Bistro Lotte</a>
+                  <a href="tel:+441373300646">01373 300646</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/VmYJyfcV7V9MS16i9" target="_blank">The George Hotel & Granary</a>
+                  <a href="tel:+441373462584">01373 462584</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/xw81VNDz4LdK8i4t9" target="_blank">Premier Inn Frome</a>
+                  <a href="tel:+443337777269">0333 777 7269</a>
+                </div>
+              </li>
+              
+              <li>
+                <div>
+                  <a href="https://www.airbnb.co.uk/s/Frome--England--United-Kingdom/homes?adults=2&checkin=2023-06-24&checkout=2023-06-25" target="_blank">Airbnb</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/hDgUqTwqewbc3WYQ9" target="_blank">Other</a>
+                </div>
               </li>
             </ul>
         </div>
       </section>
 
-      <section id="taxis">
+      <section id="taxis" className={sizeClassName}>
         <div className="section-content App__taxis">
           <span>Taxis</span>
             <ul>
               <li>
-                <a href="https://mikestaxifrome.com/">Mike Taxis</a> - <a href="tel:+447773900274">07773 900274</a>
+                <div>
+                  <a href="https://goo.gl/maps/WuyPoKYiQKodFsxD9" target="_blank">Mike Taxis</a>
+                  <a href="tel:+447773900274">07773 900274</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/LcdafUUdQ5tVqWWt8" target="_blank">Tony’s Cabs Ltd</a>
+                  <a href="tel:+441373469898">01373 469898</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/6VRWp5mLxqXUHrUs5" target="_blank">M and H Taxis</a>
+                  <a href="tel:+447768856327">07768 856327</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/Yj4MFjb5DnxYkJLT7" target="_blank">Smile Taxis</a>
+                  <a href="tel:+447931893636">07931 893636</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/Yj4MFjb5DnxYkJLT7" target="_blank">Crazy Cabs Taxi</a>
+                  <a href="tel:+441373466000">01373 466000</a>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <a href="https://goo.gl/maps/DLh3HtyBSfuTqaJa9" target="_blank">Other</a>
+                </div>
               </li>
             </ul>
         </div>
@@ -78,8 +159,8 @@ function App() {
       <section id="rsvp">
         <div className="section-content App__rsvp">
           <span>Please RSVP to</span>
-          <span className="App__rsvpEmail"><a href="mailto:ameliaandstevegettingmarried@gmail.com">ameliaandstevegettingmarried@gmail.com</a></span>
-          <span>by 31st March.</span>
+          <span className={`App__rsvpEmail ${sizeClassName}`}><a href="mailto:ameliaandstevegettingmarried@gmail.com">ameliaandstevegettingmarried@gmail.com</a></span>
+          <span>by 31st March 2023.</span>
         </div>
       </section>
 
---
+++ src/HideAppBar.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function HideAppBar(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className='HideAppBar'>
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={handleClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem className="HideAppBar__item" onClick={handleClose}><a href="#home">Home</a></MenuItem>
+              <MenuItem className="HideAppBar__item" onClick={handleClose}><a href="#itinerary">On the day</a></MenuItem>
+              <MenuItem className="HideAppBar__item" onClick={handleClose}><a href="#accomodation">Accomodation</a></MenuItem>
+              <MenuItem className="HideAppBar__item" onClick={handleClose}><a href="#taxis">Taxis</a></MenuItem>
+              <MenuItem className="HideAppBar__item" onClick={handleClose}><a href="#rsvp">RSVP</a></MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </div>
+  );
+}
